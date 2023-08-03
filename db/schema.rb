953f11ebbd143988d4501f36214a5a0fc0624672
Pavel <zelenkovpavlik@gmail.com>
2023-08-04 00:35:25 +0300

    Add table tests, question, answer, user, category

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_08_03_213250) do
+ActiveRecord::Schema.define(version: 2023_08_03_213437) do
 
   create_table "answers", force: :cascade do |t|
     t.string "body"
     t.boolean "correct"
     t.integer "question_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "categories", force: :cascade do |t|
+    t.string "title"
+    t.integer "user_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
