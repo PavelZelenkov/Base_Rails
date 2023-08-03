9238a65e395691c0d45afc5c97022bb3c9015ef4
Pavel <zelenkovpavlik@gmail.com>
2023-08-04 00:29:11 +0300

    Add table tests, question, answer

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_08_03_211800) do
+ActiveRecord::Schema.define(version: 2023_08_03_212743) do
+
+  create_table "answers", force: :cascade do |t|
+    t.string "body"
+    t.boolean "correct"
+    t.integer "question_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "questions", force: :cascade do |t|
+    t.string "body"
+    t.integer "test_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "tests", force: :cascade do |t|
     t.string "title"
