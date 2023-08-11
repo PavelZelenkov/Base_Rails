8dde7fa53eb042ebad33e0a9ba1781c839d8b49d
Pavel <zelenkovpavlik@gmail.com>
2023-08-12 00:20:57 +0300

    BAD OPTIONS

--- db/schema.rb
+++ db/schema.rb
@@ -10,42 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_08_10_210221) do
-
-  create_table "answers", force: :cascade do |t|
-    t.string "body", null: false
-    t.boolean "correct", default: false
-    t.integer "question_id", null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
-
-  create_table "categories", force: :cascade do |t|
-    t.string "title", null: false
-    t.integer "user_id", null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
+ActiveRecord::Schema.define(version: 0) do
 
   create_table "questions", force: :cascade do |t|
-    t.string "body", null: false
-    t.integer "test_id", null: false
+    t.integer "tests_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.index ["tests_id"], name: "index_questions_on_tests_id"
   end
 
-  create_table "tests", force: :cascade do |t|
-    t.string "title", null: false
-    t.integer "level", default: 0, null: false
-    t.integer "category_id", null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
-
-  create_table "users", force: :cascade do |t|
-    t.string "title", null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
-
+  add_foreign_key "questions", "tests", column: "tests_id"
 end
