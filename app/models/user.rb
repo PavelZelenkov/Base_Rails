c72db91791565940940e49a0009af704900c6281
Pavel <zelenkovpavlik@gmail.com>
2023-08-29 22:22:09 +0300

    add data set, instance method User model, Test model class method

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,10 @@
 class User < ApplicationRecord
-  has_many :categories
+  has_many :categories, dependent: :destroy
+  has_many :test_passages, dependent: :destroy
+  has_many :tests, :through => :test_passages
+
+  def ended_on_level(level)
+    tests.joins(:test_passages).where(tests: {level: level},
+    test_passages: {user_id: id})
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -14,14 +14,14 @@ users = User.create([
 
 categories = Category.create([
   {title: 'Backend', user_id: users.first.id},
-  {title: 'Frontend', user_id: users.first.id},
+  {title: 'Frontend', user_id: users[-2].id},
   {title: 'Mobile development', user_id: users.last.id}
 ])
 
 tests = Test.create([
   {title: 'Ruby', level: 0, category_id: categories.first.id},
-  {title: 'HTML', level: 1, category_id: categories.first.id},
-  {title: 'CSS', level: 1, category_id: categories.first.id},
+  {title: 'HTML', level: 1, category_id: categories[-2].id},
+  {title: 'CSS', level: 1, category_id: categories[-2].id},
   {title: 'Go', level: 0, category_id: categories.first.id},
   {title: 'Java', level: 0, category_id: categories.last.id},
   {title: 'Ruby', level: 1, category_id: categories.first.id}
@@ -29,18 +29,18 @@ tests = Test.create([
 
 questions = Question.create([
   {body: 'Вопрос 1', test_id: tests.first.id},
-  {body: 'Вопрос 2', test_id: tests.first.id},
-  {body: 'Вопрос 3', test_id: tests.first.id},
-  {body: 'Вопрос 4', test_id: tests.last.id},
-  {body: 'Вопрос 5', test_id: tests.last.id},
+  {body: 'Вопрос 2', test_id: tests[-5].id},
+  {body: 'Вопрос 3', test_id: tests[-4].id},
+  {body: 'Вопрос 4', test_id: tests[-3].id},
+  {body: 'Вопрос 5', test_id: tests[-2].id},
   {body: 'Вопрос 6', test_id: tests.last.id}
 ])
 
 answers = Answer.create([
   {body: 'Ответ 1', correct: 1, question_id: questions.first.id},
-  {body: 'Ответ 2', correct: 1, question_id: questions.first.id},
-  {body: 'Ответ 3', correct: 1, question_id: questions.first.id},
-  {body: 'Ответ 4', correct: 1, question_id: questions.last.id},
-  {body: 'Ответ 5', correct: 1, question_id: questions.last.id},
+  {body: 'Ответ 2', correct: 1, question_id: questions[-5].id},
+  {body: 'Ответ 3', correct: 1, question_id: questions[-4].id},
+  {body: 'Ответ 4', correct: 1, question_id: questions[-3].id},
+  {body: 'Ответ 5', correct: 1, question_id: questions[-2].id},
   {body: 'Ответ 6', correct: 1, question_id: questions.last.id}
 ])
