544a2430f0f400fcb9aece2719374d630cefae99
Pavel <zelenkovpavlik@gmail.com>
2023-09-07 01:17:09 +0300

    Add associations with comments

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,8 +1,8 @@
 class Test < ApplicationRecord
-  belongs_to :category
-  has_many :questions, dependent: :destroy
-  has_many :test_passages, dependent: :destroy
-  has_many :users, through: :test_passages
+  belongs_to :category # связь с моделью category
+  has_many :questions, dependent: :destroy # связь с моделями questions
+  has_many :test_passages, dependent: :destroy # связь с промежуточной моделью test_passage
+  has_many :users, through: :test_passages # связь с моделью user через промежуточную модель test_passage
 
   def self.test_with_category(category)
     Test.joins(:category).where(categories: {title: category}).order(title: :desc).pluck(:title)
--- app/models/test_passage.rb
+++ app/models/test_passage.rb
@@ -1,4 +1,4 @@
 class TestPassage < ApplicationRecord
-  belongs_to :user
-  belongs_to :test
+  belongs_to :user # связь с моделью user
+  belongs_to :test # связь с моделью test
 end
