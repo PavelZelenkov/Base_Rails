e6d59cd860f2baa5ccc11dedca1c83c8b7e206d5
Pavel <zelenkovpavlik@gmail.com>
2023-09-23 00:30:39 +0300

    Add uniqueness at the database level and fixes

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -2,9 +2,9 @@ class Answer < ApplicationRecord
   belongs_to :question
 
   validates :body, presence: true
-  validate :num_of_answers
+  validate :num_of_answers_for_questions
 
-  def num_of_answers
+  def num_of_answers_for_questions
     errors.add(:question) if question.answers.count >= 4
   end
 end
--- app/models/test.rb
+++ app/models/test.rb
@@ -6,17 +6,21 @@ class Test < ApplicationRecord
   belongs_to :author, class_name: 'User', foreign_key: :author_id
 
   validates :title, presence: true
-  validates :level, numericality: { only_integer: true }
   validates :title, uniqueness: { scope: :level,
     message: "С данным уровнем тест уже существует" }
-
-  scope :easy, -> { where(level: 0..1) }
-  scope :normal, -> { where(level: 2..4) }
-  scope :heavy, -> { where(level: 5..Float::INFINITY) }
+  validates :level, numericality: { only_integer: true }
 
   scope :level_selection, -> (level) { where(level: level) }
 
-  default_scope { order(title: :asc) }
+  scope :easy, -> { level_selection(0..1) }
+  scope :normal, -> { level_selection(2..4) }
+  scope :heavy, -> { level_selection(5..Float::INFINITY) }
 
-  scope :with_category, -> (category) { joins(:category).where(categories: {title: category}).pluck(:title) }
+  # default_scope { order(title: :asc) } # закомментировал данный скоуп и эту сортировку добавил в метод класса
+
+  scope :with_category, -> (category) { joins(:category).where(categories: {title: category})}
+
+  def self.test_with_category(category)
+    Test.with_category(category).order(title: :asc).pluck(:title)
+  end
 end
