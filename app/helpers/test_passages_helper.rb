403a3fb1bc5b59da3958328b67ec4286106c3c9a
Pavel <zelenkovpavlik@gmail.com>
2023-11-19 23:25:06 +0300

    Fix test passage v4

--- app/helpers/test_passages_helper.rb
+++ app/helpers/test_passages_helper.rb
@@ -1,19 +1,2 @@
 module TestPassagesHelper
-
-  def color_result
-    if @test_passage.success?
-      "color:#32CD32"
-    else
-      "color:#ff0000"
-    end
-  end
-
-  def result_test
-    if @test_passage.success?
-      "Result: #{@test_passage.percentage_formula}% Successful completion of the test"
-    else
-      "Result: #{@test_passage.percentage_formula}% Test failed"
-    end
-  end
-
 end
