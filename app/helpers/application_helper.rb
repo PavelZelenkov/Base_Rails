073bc241fdc1b8dfcab3659f65af2a44770d8529
Pavel <zelenkovpavlik@gmail.com>
2023-11-29 16:16:52 +0300

    fix2 (flash helper) - authentication and user sessions

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -8,8 +8,8 @@ module ApplicationHelper
     link_to "GitHub-репозиторий проекта", "https://www.github.com/#{author}/#{repo}/tree/master", target: :blank
   end
 
-  def flash_message(alert)
-    content_tag :p, flash["#{alert}"], class: "flash #{alert}" if flash["#{alert}"]
+  def flash_message(flash_type)
+    content_tag :p, flash["#{flash_type}"], class: "flash #{flash_type}" if flash["#{flash_type}"]
   end
 
 end
