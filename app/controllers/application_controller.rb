918c6bc2d7201d32be6e381fbbd61b165a656b57
Pavel <zelenkovpavlik@gmail.com>
2023-11-29 16:14:11 +0300

    fix (flash helper) - authentication and user sessions

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,8 +3,7 @@ class ApplicationController < ActionController::Base
   before_action :authenticate_user!
   
   helper_method :current_user,
-                :logged_in?,
-                :allert_flash_helper
+                :logged_in?
 
   private
 
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -9,7 +9,7 @@ module ApplicationHelper
   end
 
   def flash_message(alert)
-    content_tag :p, flash[:alert], class: 'flash alert' if flash[:alert]
+    content_tag :p, flash["#{alert}"], class: "flash #{alert}" if flash["#{alert}"]
   end
 
 end
