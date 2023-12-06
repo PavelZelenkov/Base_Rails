8462f7ea46854049a281efb1641cdfc7b7d95124
Pavel <zelenkovpavlik@gmail.com>
2023-12-06 22:39:13 +0300

    Fix1 - Devise and access for the user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,7 +11,7 @@ class ApplicationController < ActionController::Base
   end
 
   def after_sign_in_path_for(resource)
-    if current_user.admin?
+    if resource.admin?
       admin_tests_path
     else
       stored_location_for(resource) || tests_path
