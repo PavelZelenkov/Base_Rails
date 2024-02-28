18e332bd07332045d0af4e0d4f3ae91127eb6155
Pavel <zelenkovpavlik@gmail.com>
2024-02-28 21:02:08 +0300

    add JavaScript, sorting, input

---
+++ app/javascript/packs/utilities/input.js
@@ -0,0 +1,15 @@
+document.addEventListener('turbolinks:load', function() {
+  var control1 = document.getElementById("user_password");
+  var control2 = document.getElementById("user_password_confirmation");
+
+  if (control2) { control2.addEventListener('input', function() {
+    if (control2.value.trim().length === 0) {
+      control2.style.backgroundColor = "transparent"
+    } else if (control1.value === control2.value) {
+      control2.style.backgroundColor = "LimeGreen"
+    } else {
+      control2.style.backgroundColor = "Red"
+    };
+  }) };
+  
+});
