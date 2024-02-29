981d3158eabead6d9a3d823f7550447a2f427fcc
Pavel <zelenkovpavlik@gmail.com>
2024-02-29 20:43:39 +0300

    fix2 - add JavaScript, sorting, input

--- app/javascript/packs/utilities/input.js
+++ app/javascript/packs/utilities/input.js
@@ -1,16 +1,21 @@
 document.addEventListener('turbolinks:load', function() {
-  var control1 = document.getElementById("user_password");
   var control2 = document.getElementById("user_password_confirmation");
 
-  if (control2) { control2.addEventListener('input', function() {
-    if (control2.value.trim().length === 0) {
-      return (control2.style.backgroundColor = "transparent")
-    };
-    if (control1.value === control2.value) {
-      control2.style.backgroundColor = "LimeGreen"
-    } else {
-      control2.style.backgroundColor = "Red"
-    }})
-  };
+  if (control2) { control2.addEventListener('input', passwordConfirmation) }
 
 });
+
+function passwordConfirmation() {
+  input1 = document.getElementById("user_password");
+  input2 = document.getElementById("user_password_confirmation");
+
+  if (input2.value.trim().length === 0) {
+    return (input2.style.backgroundColor = "transparent")
+  };
+
+  if (input1.value === input2.value) {
+    input2.style.backgroundColor = "LimeGreen"
+  } else {
+    input2.style.backgroundColor = "Red"
+  };
+};
